d82d6a32c57356f6810e6739ddf85189a1ddeee8
Kevin Ndung'u <kevgathuku@gmail.com>
2020-04-09 21:59:27 +0300

    Register models on the admin site

--- valuation/admin.py
+++ valuation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Company, BalanceSheet, IncomeStatement
+
+admin.site.register(Company)
+admin.site.register(BalanceSheet)
+admin.site.register(IncomeStatement)
